1f7257b55ffb4ad7f8e72241e85fc0b84810232d
ali-ranah <ranasaimali1234@gmail.com>
2025-04-28 12:07:09 +0500

    Made a simple hello world extension

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+        <h1 style={{fontFamily:'monospace'}}>Hello World</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
